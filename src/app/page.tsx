b341dd55da2032cc9d5efcea87782566eba6f554
ridwansaputra <riidwanadiit@gmail.com>
2025-06-14 20:16:59 +0700

    fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-// import PollRoom from "@/features/poll/views/PollRoom";
+'use client'
 
-const PollRoom = dynamic(() => import("@/features/poll/views/PollRoom"), {
-  ssr: false,
-});
+import PollRoom from "@/features/poll/views/PollRoom";
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <main className="min-h-screen flex items-center justify-center p-8">
-        <PollRoom />
-      </main>
-    </Suspense>
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <PollRoom />
+    </main>
   );
 }
